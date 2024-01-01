0ed034699e2ace90bab92605c28525f95a5626ec
Oluwatosin Akande <aktosin1@gmail.com>
2024-01-01 18:02:19 +0100

    fix: change folder structure

---
+++ go-syntax-practice/word-count.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+
+
+func wordCount(str string) map[string]int {
+	wordList := strings.Fields(str)
+	count := make(map[string]int)
+	for _, word := range wordList {
+		
+	}
+}
+
+func main() {
+	
+}
